4ed9b3e73f76b38470ee6e562d5be5d610dc2427
yeşimmmmm <yesimkaya0202@gmail.com>
2024-01-12 02:52:38 +0300

    Add files via upload

---
+++ Kullanıcıdan alınan iki sayının toplamını bulan kod.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Kullanýcýdan alýnan iki sayýnýn toplamýný bulan C kodu: */
+
+int main() {
+    int x,y,Toplam;
+	printf("1.sayiyi giriniz:");
+	scanf("%d",&x);
+	printf("2.sayiyi giriniz:");
+	scanf("%d",&y);
+	Toplam=x+y;
+	printf("%d ve %d sayilarinin toplami:%d",x,y,Toplam);	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
