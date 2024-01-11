8a5745a6bd649a1edb2622bc5de08c6739ae16b4
yeşimmmmm <yesimkaya0202@gmail.com>
2024-01-12 02:53:23 +0300

    Add files via upload

---
+++ Kullanıcıdan alınan ismi yine kullanıcıdan alına sayıya göre ekrana yazdıran C kodu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Kullanýcýdan alýnan ismi yine kullanýcýdan alýna sayýya göre ekrana yazdýran C kodu:*/
+
+int main() {
+	
+	int i,sayi;
+	char isim;
+	printf("Lutfen isminizi giriniz:");
+	scanf("Isim:%s",&isim);
+	
+	printf("Kac defa yazdirmak istersiniz:");
+	scanf("Sayi:%d",&sayi);
+	
+
+	return 0;
+}
