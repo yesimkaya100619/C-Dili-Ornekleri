b0c11793e8570d5dd9e20f3a687e76a5f45c104b
yeşimmmmm <yesimkaya0202@gmail.com>
2024-01-12 02:48:02 +0300

    Add files via upload

---
+++ Kullanıcıdan alınan cümlenin parçalayıp alt alta bölen C kodu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Kullanýcýdan alýnan cümlenin parçalayýp alt alta bölen C kodu: */
+}
+int main() {
+	
+	char cumle[100];
+	int i,j;
+	printf("Bir cumle giriniz:");
+	gets(cumle);
+	
+	for(i=0;i<cumle[i]!='\0';i++){
+		for(j=0;j<=i;j++){
+		}
+		printf("%c\n",cumle[i]);
+	}
+	
+	
+
+	return 0;
+}
