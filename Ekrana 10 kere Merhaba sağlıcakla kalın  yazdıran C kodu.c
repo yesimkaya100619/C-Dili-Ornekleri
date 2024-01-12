31392f727420940bbfa2670e7280804c721f2386
yeşimmmmm <yesimkaya0202@gmail.com>
2024-01-12 03:03:23 +0300

    Add files via upload

---
+++ Ekrana 10 kere Merhaba sağlıcakla kalın  yazdıran C kodu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Ekrana 10 kere "Merhaba saðlýcakla kalýn " yazdýran C kodu: */
+
+int main() {
+	int sayi;
+	for(sayi=0;sayi<=9;sayi++){
+		printf("%d- Merhaba saglicakla kalin\n",sayi);
+	}
+	
+	
+	printf("\n\n*****TEBRIKLER ISLEM BASARILI*****");
+
+	return 0;
+}
