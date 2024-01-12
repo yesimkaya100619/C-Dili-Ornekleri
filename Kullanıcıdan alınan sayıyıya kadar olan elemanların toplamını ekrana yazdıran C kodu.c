75ffbf2f286c7c0b47c3a7fe651c3d85e277c2d5
yeşimmmmm <yesimkaya0202@gmail.com>
2024-01-12 03:01:00 +0300

    Add files via upload

---
+++ Kullanıcıdan alınan sayıyıya kadar olan elemanların toplamını ekrana yazdıran C kodu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Kullanýcýdan alýnan sayýyýya kadar olan elemanlarýn toplamýný ekrana yazdýran C kodu:*/
+
+int main() {
+	
+	
+	int i,sayi;
+	
+	for(i=0;i<5;i++){
+		printf("Lutfen Bir Sayi Giriniz:");
+		scanf("%d",&sayi);
+	}
+	
+
+	
+	return 0;
+}
